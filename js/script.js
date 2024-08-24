8dda6e61cab01b12dac5cb9eb449e2f75985bab5
RevanoAugustofa <revanoa4945@gmail.com>
2024-08-24 20:58:44 +0700

    update

--- js/script.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('.sidebar-dropdown-toggle').forEach(function(item){
-    item.addEventListener('click',function(e){ 
-        e.preventDefault()
-        const parent = item.closest('.group')
-        parent.classlist.add('selected')    
-    })
-})
