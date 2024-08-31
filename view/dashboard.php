68b82c259bf6471be70264009ae27c93ef255805
RevanoAugustofa <revanoa4945@gmail.com>
2024-08-31 15:30:34 +0700

    update

--- view/dashboard.php
+++ view/dashboard.php
@@ -106,10 +106,10 @@ $db = new database;
                     </figure>
                     <div class="card-body">
                         <div class="flex justify-between">
-                        <h2 class="card-title">Prodi</h2>
+                        <h2 class="card-title">Program Studi</h2>
                         <i class="ri-contacts-book-2-fill text-2xl"></i>
                         </div>
-                        <p>Jumlah Prodi</p>
+                        <p>Jumlah Program Studi</p>
                         <p class="text-3xl font-bold"><?= $db->getalldata('prodi') ?></p>
                     </div>
                 </div>
@@ -345,97 +345,6 @@ $db = new database;
                 });
             });
         });
-
-
-        
-// const options = {
-//     // add data series via arrays, learn more here: https://apexcharts.com/docs/series/
-//     series: [
-//       {
-//         name: "Masuk",
-//         data: [1494, 1418, 1256, 1456, 1479, 1356],
-//         color: "#1A56DB",
-//       },
-//       {
-//         name: "Tidak Masuk",
-//         data: [6, 82, 244, 44, 21, 144],
-//         color: "#7E3BF2",
-//       },
-//     ],
-//     chart: {
-//       height: "100%",
-//       maxWidth: "100%",
-//       type: "area",
-//       fontFamily: "Inter, sans-serif",
-//       dropShadow: {
-//         enabled: false,
-//       },
-//       toolbar: {
-//         show: false,
-//       },
-//     },
-//     tooltip: {
-//       enabled: true,
-//       x: {
-//         show: false,
-//       },
-//     },
-//     legend: {
-//       show: false
-//     },
-//     fill: {
-//       type: "gradient",
-//       gradient: {
-//         opacityFrom: 0.55,
-//         opacityTo: 0,
-//         shade: "#1C64F2",
-//         gradientToColors: ["#1C64F2"],
-//       },
-//     },
-//     dataLabels: {
-//       enabled: false,
-//     },
-//     stroke: {
-//       width: 6,
-//     },
-//     grid: {
-//       show: false,
-//       strokeDashArray: 4,
-//       padding: {
-//         left: 2,
-//         right: 2,
-//         top: 0
-//       },
-//     },
-//     xaxis: {
-//       categories: ['01 February', '02 February', '03 February', '04 February', '05 February', '06 February', '07 February'],
-//       labels: {
-//         show: false,
-//       },
-//       axisBorder: {
-//         show: false,
-//       },
-//       axisTicks: {
-//         show: false,
-//       },
-//     },
-//     yaxis: {
-//       show: false,
-//       labels: {
-//         formatter: function (value) {
-//           return '' + value;
-//         }
-//       }
-//     },
-//     }
-    
-//     if (document.getElementById("data-series-chart") && typeof ApexCharts !== 'undefined') {
-//     const chart = new ApexCharts(document.getElementById("data-series-chart"), options);
-//     chart.render();
-//     }
-    
-
-        
 
 
     </script>
